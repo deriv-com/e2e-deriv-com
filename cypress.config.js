19942e5734a91658c68fdaaf0c10e5c6ad292242
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2023-09-19 12:33:10 +0100

    Markodonnell deriv patch 2 (#9)
    * Update cypress.config.js
    
    Updated BaseUrl
    
    * Latest best pracitise strucure.

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,16 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: 'https://deriv.com',
+    //baseUrl: 'https://deriv.com',
     defaultCommandTimeout: 10000,
+    //iPhone14
+    viewportWidth: 393,
+    viewportHeight: 852,
     supportFile: "cypress/support/e2e.js"
   },
   env: {
+    baseUrl: 'https://deriv.com',
     email: "test@example.com",
+    isMobile: "true"
   },
 })
