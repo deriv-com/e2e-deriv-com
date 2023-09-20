0830c4d5caf066cc355212e87ef8cd3590764f02
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2023-09-20 12:18:50 +0100

    Update markets.cy.js

--- cypress/e2e/markets.cy.js
+++ cypress/e2e/markets.cy.js
@@ -1,14 +1,16 @@
 import '@testing-library/cypress/add-commands'
 
 describe('Markets link tests', () => {
-    beforeEach(() => {
+      if (Cypress.env('isMobile')) {
+        cy.viewport(393,852)
+      }
       cy.visit('/')
       cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible') 
     })
 
     it('Markets - menu', () => {
       //tag: smoke
-      if (Cypress.env('isMobile') == 'true')
+      if (Cypress.env('isMobile'))
       {
         //Click through 'Markets' menu-items. 
         cy.findByRole('img', { name: 'hamburger menu' }).click()
