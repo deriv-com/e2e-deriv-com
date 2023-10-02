db50bd942c174ab8c87b04668e6cf462ad49e628
markodonnell-deriv <144238191+markodonnell-deriv@users.noreply.github.com>
2023-10-02 07:46:45 +0100

    Further mods and new tests (#27)

--- cypress/e2e/smoke/home.cy.js
+++ cypress/e2e/smoke/home.cy.js
@@ -2,15 +2,53 @@ import '@testing-library/cypress/add-commands'
 
 describe('QAA-663 - Navigation Responsive - Menu items - EU and ROW', () => {
 
-    //Click through 'Markets' menu-items and ensure links are vaild and load the next page. 
+    //Click through menu-items and ensure links are vaild and load the next page. 
     it('Generic menu items', () => {
-      cy.visitResponsive(Cypress.env('RegionEU'), 'small') //See custome command for details
-      cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible') //A temporary work around, as menu items are not always visible and I don't want to have to do a {force true} on all of the methods.
+      cy.visitResponsive(Cypress.env('RegionEU'), 'small') //See custom command for details
+
+      //Trade
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Trade chevron' }).click();
+      cy.findByRole('link', { name: 'CFDs trade type CFDs Trade with leverage and tight spreads for better returns on successful trades.' }).click()
+      cy.findByRole('heading', { name: 'CFD trading', exact: true }).should('be.visible')
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Trade chevron' }).click({force: true})
+      cy.findByRole('link', { name: 'Options trade type Options Earn a range of payouts without risking more than your initial stake.' }).should('not.exist')
+
+      cy.findByRole('button', { name: 'Trade chevron' }).click();
+      cy.findByRole('link', { name: 'Multipliers trade type Multipliers Get the upside of CFDs without the downside of losing more than your stake.' }).click()
+      cy.findByRole('heading', { name: 'Multipliers', exact: true }).should('be.visible')
+
+      //Markets
 
       cy.findByRole('img', { name: 'hamburger menu' }).click()
       cy.findByRole('button', { name: 'Markets chevron' }).click({force: true})
       cy.findByRole('link', { name: 'Forex Forex Trade the world’s largest financial market with popular forex pairs.' }).click()
       cy.findByRole('heading', { name: 'Forex', exact: true }).should('be.visible')
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Markets chevron' }).click({force: true})
+      cy.findByRole('link', { name: 'Synthetic indices Derived indices Enjoy trading asset prices derived from simulated markets.' }).click()
+      cy.findByRole('heading', { name: 'Derived indices' }).should('be.visible')
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Markets chevron' }).click({force: true})
+      cy.findByRole('link', { name: 'Stocks & indices Stocks & indices Predict broader market trends and diversify your risk with stocks & indices.' }).click()
+      cy.findByRole('heading', { name: 'Stocks & indices', exact: true }).should('be.visible')
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Markets chevron' }).click({force: true})
+      cy.findByRole('link', { name: 'Commodities Commodities Trade natural resources that are central to the world\'s economy.' }).click()
+      cy.findByRole('heading', { name: 'Commodities', exact: true }).should('be.visible')
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Markets chevron' }).click({force: true})
+      cy.findByRole('link', { name: 'Cryptocurrencies Cryptocurrencies Trade with leverage on the price movement of popular crypto-fiat pairs.' }).click()
+      cy.findByRole('heading', { name: 'Cryptocurrencies', exact: true }).should('be.visible')
+
+      //About us
 
       cy.findByRole('img', { name: 'hamburger menu' }).click()
       cy.findByRole('button', { name: 'About us chevron' }).click()
@@ -22,20 +60,108 @@ describe('QAA-663 - Navigation Responsive - Menu items - EU and ROW', () => {
       cy.findByRole('link', { name: 'Why choose us' }).click()
       cy.findByRole('heading', { name: 'Why choose us' }).should('be.visible')
 
-    })
-    //Click through 'Markets' menu-items and ensure links are vaild and load the next page. 
+          //--> TODO - Partnership programmes
 
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'About us chevron' }).click()
+      cy.findByRole('link', { name: 'Contact us' }).click()
+      cy.findByRole('heading', { name: 'Contact us' }).should('be.visible')
+
+          //--> TODO - Careers
+
+
+      //Resources
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Resources chevron' }).click()
+      cy.findByRole('link', { name: 'Help centre' }).click()
+      cy.findByRole('heading', { name: 'How can we help?' }).should('be.visible')
+
+          //--> TODO - Community
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Resources chevron' }).click()
+      cy.findByRole('link', { name: "Traders’ tools" }).click();
+      cy.findByRole('heading', { name: "Traders’ tools" }).should('be.visible')
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Resources chevron' }).click()
+      cy.findByRole('link', { name: 'Payment methods', exact: true }).click()
+      cy.findByRole('heading', { name: 'Payment methods' }).should('be.visible')
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Resources chevron' }).click()
+      cy.findByRole('link', { name: 'Deriv MT5 Signals' }).click()
+      cy.findByRole('heading', { name: 'Deriv MT5 signals' }).should('be.visible')
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Resources chevron' }).click()
+      cy.findByRole('link', { name: 'Status page' }).click()
+      cy.findByRole('button', { name: 'Proceed' }).should('be.visible')
+
+      //Legal
+
+      cy.visitResponsive(Cypress.env('RegionEU'), 'small') //See custom command for details
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Legal chevron' }).click()
+      cy.findByRole('link', { name: 'Regulatory information' }).click()
+      cy.findByRole('heading', { name: 'Regulatory information' }).should('be.visible')
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Legal chevron' }).click()
+      cy.findByLabelText('Legal').findByRole('link', { name: 'Terms and conditions' }).click()
+      cy.findByRole('heading', { name: 'Terms and conditions' }).should('be.visible')
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Legal chevron' }).click()
+      cy.findByLabelText('Legal').findByRole('link', { name: 'Secure and responsible trading' }).click()
+      cy.findByRole('heading', { name: 'Secure and responsible trading' }).should('be.visible')
+
+      //Partners
+
+      cy.findByRole('img', { name: 'hamburger menu' }).click()
+      cy.findByRole('button', { name: 'Partners chevron' }).click()
+      cy.findByRole('link', { name: 'Deriv Prime' }).click()
+      cy.findByRole('heading', { name: 'Global liquidity for all' }).should('be.visible')
+
+                //--> TODO - Affiliates
+      // cy.findByRole('img', { name: 'hamburger menu' }).click()
+      // cy.findByRole('button', { name: 'Partners chevron' }).click()
+      // cy.findByRole('link', { name: 'Affiliates' }).click()
+      // cy.findByRole('heading', { name: 'Partner with a trusted online trading provider' }).should('be.visible')
+
+                //--> TODO - Deriv API
+      // cy.visitResponsive(Cypress.env('RegionEU'), 'small') //See custom command for details
+      // cy.findByRole('img', { name: 'hamburger menu' }).click()
+      // cy.findByRole('button', { name: 'Partners chevron' }).click()
+      // cy.findByRole('link', { name: 'API', exact: true }).click()
+      // cy.findByRole('heading', { name: 'Deriv API', exact: true }).should('be.visible')
+
+            //--> TODO - Community
+
+
+    })
+
+    //Click through menu-items and ensure links are vaild and load the next page. 
     it('ROW menu items', () => {
 
-      //TODO - We need to be able to simulate either VPN in GH run in a non-eu reigon.
+      //NB. The region switch doesn't work on the EU site at the moment.
+      if (Cypress.env('skipROWTests') == false)
+        {
+        cy.visitResponsive(Cypress.env('RegionROW'), 'small') //See custom command for details
+
+        cy.findByRole('img', { name: 'hamburger menu' }).click()
+        cy.findByRole('button', { name: 'Trade chevron' }).click({force: true})
+        cy.findByRole('link', { name: 'Options trade type Options Earn a range of payouts without risking more than your initial stake.' }).click()
+        cy.findByRole('heading', { name: 'What are digital options?' }).should('be.visible')
+        }
 
     })
 })
 
 describe('QAA-664 - Navigation Responsive - Open/Close Menu', () => {
     beforeEach(() => {
-      cy.visitResponsive('/', 'small') //See custome command for details
-      cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible') //A temporary work around, as menu items are not always visible and I don't want to have to do a {force true} on all of the methods.
+      cy.visitResponsive(Cypress.env('RegionEU'), 'small') //See custom command for details
     })
 
     it('Validate hamburger menu operation', () => {
@@ -61,8 +187,7 @@ describe('QAA-664 - Navigation Responsive - Open/Close Menu', () => {
 
 describe('QAA-665 - Validate Footer and Popup Icons', () => {
     beforeEach(() => {
-      cy.visitResponsive('/') //See custome command for details
-      cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible') //A temporary work around, as menu items are not always visible and I don't want to have to do a {force true} on all of the methods.
+      cy.visitResponsive(Cypress.env('RegionEU')) //See custom command for details
     })
     it('Validate footer exists', () => {
       cy.get('footer').should('exist')
@@ -80,8 +205,8 @@ describe('QAA-665 - Validate Footer and Popup Icons', () => {
 
 describe('QAA-668 - Validate Spanish Language Change', () => {
   beforeEach(() => {
-    cy.visitResponsive('/') //See custome command for details
-    cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible') //A temporary work around, as menu items are not always visible and I don't want to have to do a {force true} on all of the methods.
+    cy.visitResponsive(Cypress.env('RegionEU')) //See custom command for details
+    
   })
 
   it('Select Spanish and check with header changed', () => {
@@ -93,22 +218,27 @@ describe('QAA-668 - Validate Spanish Language Change', () => {
   describe('QAA-670 - Validate Hero banner message EU and ROW', () => {
   
     it('EU', () => {
-      cy.visitResponsive(Cypress.env('RegionEU')) //See custome command for details
-      cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible') //A temporary work around, as menu items are not always visible and I don't want to have to do a {force true} on all of the methods.
+      cy.visitResponsive(Cypress.env('RegionEU')) //See custom command for details
 
       cy.findByTestId('typewriter-wrapper').findByText('Forex').should('be.visible')
-      cy.findByTestId('typewriter-wrapper').findByText('Cryptocurrencie').click();
-      cy.findByTestId('typewriter-wrapper').findByText('Stocks & indices').click();
-      cy.findByTestId('typewriter-wrapper').findByText('Comm').click();
-      cy.findByTestId('typewriter-wrapper').findByText('Derive').click();
+      cy.findByTestId('typewriter-wrapper').findByText('Cryptocurrencie').click()
+      cy.findByTestId('typewriter-wrapper').findByText('Stocks & indices').click()
+      cy.findByTestId('typewriter-wrapper').findByText('Comm').click()
+      cy.findByTestId('typewriter-wrapper').findByText('Derive').click()
     })
 
     it('ROW', () => {
-      cy.visitResponsive(Cypress.env('RegionROW')) //See custome command for details
-      cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible') //A temporary work around, as menu items are not always visible and I don't want to have to do a {force true} on all of the methods.
+      if (Cypress.env('skipROWTests') == false)
+      {
+        //NB. The region switch doesn't work on the EU site at the moment.
+        cy.visitResponsive(Cypress.env('RegionROW')) //See custom command for details
+        //cy.findByTestId('typewriter-wrapper').findByText('ETFs').click() //? The test case mentions ETFs but the banner doesn't appear to be different to EU
+      }
+      
 
-      //TODO - We need to be able to simulate either VPN in GH run in a non-eu reigon.
+      
     })
 
   })
+
 })
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -10,8 +10,18 @@ Cypress.Commands.add('visitResponsive', (path, size) => {
         cy.viewport('ipad-2')
     else
         cy.viewport('macbook-16')
+
     cy.visit(path)
+
+    if (path.includes('region')) //Wait for relevent elements to appear (based on page)
+        {
+            cy.log('Home page Selected')
+            cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible', { timeout: 30000 }) //For the home page, this seems to be the best indicator that a page has fully loaded. It may change in the future.
+        }
+
 });
+
+
 
 
 
