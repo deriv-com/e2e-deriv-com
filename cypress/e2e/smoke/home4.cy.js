c45d5b69fb167fe358710f373fa41e7fb0b79b2b
Fasih Ali <fasihali@Fasih-Alis-MacBook-Pro-C02DJ87UMD6M.local>
2023-11-01 17:30:07 +0800

    fix: fixed test for ROW

--- cypress/e2e/smoke/home4.cy.js
+++ cypress/e2e/smoke/home4.cy.js
@@ -8,11 +8,11 @@ describe('Validate Hero banner message', () => {
     {
       //TODO - Find out what should be different here otherwise, repeating the same tests seems unnecessary
       cy.c_visitResponsive(Cypress.env('RegionROW'))
-      cy.findByTestId('typewriter-wrapper').findByText('Fo').should('be.visible')
-      cy.findByTestId('typewriter-wrapper').findByText('Cr').should('be.visible')
-      cy.findByTestId('typewriter-wrapper').findByText('St').should('be.visible')
-      cy.findByTestId('typewriter-wrapper').findByText('Co').should('be.visible')
-      cy.findByTestId('typewriter-wrapper').findByText('De').should('be.visible')
+      cy.findByTestId('typewriter-wrapper').findByText('Forex').should('be.visible')
+      cy.findByTestId('typewriter-wrapper').findByText('Cryptocurrencies').should('be.visible')
+      cy.findByTestId('typewriter-wrapper').findByText('Stocks & indices').should('be.visible')
+      cy.findByTestId('typewriter-wrapper').findByText('Commodities').should('be.visible')
+      cy.findByTestId('typewriter-wrapper').findByText('Derived Indices').should('be.visible')
       cy.findByLabelText('create free demo account').click()
       cy.findByRole('heading', { name: 'Sign up' }).should('be.visible')
     }      
