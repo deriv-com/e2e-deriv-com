3c55e1a49afeb5e20189f2913bb99e458533cf4f
fasihali-deriv <121229483+fasihali-deriv@users.noreply.github.com>
2023-11-07 10:41:26 +0800

    Fasih/: Updated full keywords (#40)
    * fix: Updated full keywords
    
    * fix: fixed test for ROW
    
    ---------
    
    Co-authored-by: Fasih Ali <fasihali@Fasih-Alis-MacBook-Pro-C02DJ87UMD6M.local>

--- cypress/e2e/smoke/home3.cy.js
+++ cypress/e2e/smoke/home3.cy.js
@@ -5,11 +5,11 @@ describe('Validate Hero banner message', () => {
   it('QATEST-1315 - EU', () => {
     cy.c_visitResponsive(Cypress.env('RegionEU'))
 
-    cy.findByTestId('typewriter-wrapper').findByText('Fo').should('be.visible')
-    cy.findByTestId('typewriter-wrapper').findByText('Cr').should('be.visible')
-    cy.findByTestId('typewriter-wrapper').findByText('St').should('be.visible')
-    cy.findByTestId('typewriter-wrapper').findByText('Co').should('be.visible')
-    cy.findByTestId('typewriter-wrapper').findByText('De').should('be.visible')
+    cy.findByTestId('typewriter-wrapper').findByText('Forex').should('be.visible')
+    cy.findByTestId('typewriter-wrapper').findByText('Cryptocurrencies').should('be.visible')
+    cy.findByTestId('typewriter-wrapper').findByText('Stocks & indices').should('be.visible')
+    cy.findByTestId('typewriter-wrapper').findByText('Commodities').should('be.visible')
+    cy.findByTestId('typewriter-wrapper').findByText('Derived Indices').should('be.visible')
     cy.findByLabelText('create free demo account').click()
     cy.findByRole('heading', { name: 'Sign up' }).should('be.visible')
   })
