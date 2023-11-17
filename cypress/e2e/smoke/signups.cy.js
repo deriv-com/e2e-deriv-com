569e5b3d491523c92a4725b93f23e7425bcb8f2f
pravin-deriv <103259298+pravin-deriv@users.noreply.github.com>
2023-11-17 12:21:23 +0800

    Pravin/signup (#51)
    * try first push
    
    * try first 1
    
    * signup page validation for EU and Non-EU
    
    * amended test case name and added ;
    
    * ammend spacing

--- cypress/e2e/smoke/signups.cy.js
+++ cypress/e2e/smoke/signups.cy.js
@@ -1,12 +1,32 @@
 import '@testing-library/cypress/add-commands'
 
-describe('Validate signup links', () => {
-  beforeEach(() => {
-    cy.c_visitResponsive('/signup') 
-  })
+describe('QATEST-1378 - Should validate signup page', () => {
+  const validateSignup = () => {
+    cy.findByRole('heading', { name: 'Sign up' }).should('be.visible');
+    
+    cy.findByPlaceholderText('Email').should('be.visible');
+    cy.findByLabelText('I agree to the terms and conditions').should('be.visible');
+    cy.contains('button', 'Create demo account').should('be.visible');
+    
+    cy.findByRole('button', { name: 'Google' }).should('be.visible');
+    cy.findByRole('button', { name: 'Facebook' }).should('be.visible');
+    cy.findByRole('button', { name: 'Apple' }).should('be.visible');
+    
+    cy.contains('Already have an account? Log in').should('be.visible');
+  };
 
-  it('Validate user can signup at EU/non-EU Website', () => {
-    cy.findByLabelText('I agree to the terms and conditions').should('be.visible')
-    cy.contains('button', 'Create demo account').should('be.visible')
-  })
+  it('Should be able to sign up on a NON-EU website', () => {
+    cy.c_visitResponsive('/signup' + String(Cypress.env('RegionROW')));
+    validateSignup();
+  });
+
+  it('Should be able to sign up on a EU website', () => {
+    cy.c_visitResponsive('/signup' + String(Cypress.env('RegionEU')));
+    validateSignup();
+    // EU-specific footer
+    cy.findByRole('link', { name: '71% of retail investor accounts lose money when trading CFDs with Deriv, read our full Risk disclosure here.' }).should('be.visible');
+  });
+
+
+  
 })
