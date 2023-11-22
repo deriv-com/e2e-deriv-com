8105a202e38db27318c424bdf836b81b3260a60a
Rivitha <104499245+infanta-deriv@users.noreply.github.com>
2023-11-22 16:24:45 +0400

    fixinconfigfile (#66)

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,4 +1,4 @@
-import "./commands";
+import "./livepricing";
 
 Cypress.Commands.add("c_visitResponsive", (path, size) => {
   //Custom command that allows us to use baseUrl + path and detect with this is a responsive run or not.
