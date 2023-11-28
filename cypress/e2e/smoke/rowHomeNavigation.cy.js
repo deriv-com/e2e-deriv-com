35b322271513d13705ddbf8346c7df62981815e7
shariqh-deriv <shariqh@deriv.com>
2023-11-28 18:06:06 +0400

    Home page Navigation ROW

---
+++ cypress/e2e/smoke/rowHomeNavigation.cy.js
@@ -0,0 +1,24 @@
+import '@testing-library/cypress/add-commands'
+
+describe('QATEST-1266 - deriv.com ROW home page should be opened with the url as same as deriv.com', () => {
+    const checkDerivComROWLayout = (layout) => {
+        cy.c_visitResponsive(Cypress.env('RegionROW'), 'small'); 
+        const baseUrl = Cypress.config('baseUrl');
+        const urlToMatch = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl;
+        cy.url().should('eq', urlToMatch + Cypress.env('RegionROW'));
+        cy.findByRole('heading', { name: 'Options' }).should('be.visible');   // checking Option trade type is present on ROW
+        cy.findByRole('img', { name: 'p2p banner' });                          // checking p2p section is present  on ROW
+    };
+
+    it('Should open url deriv.com and check ROW HomePage opens in mobile', ()=> {
+        if (Cypress.env('skipROWTests') == false) {
+            checkDerivComROWLayout('small');
+        }
+    });
+
+    it('Should open url deriv.com and check ROW HomePage opens in desktop', ()=> {
+        if (Cypress.env('skipROWTests') == false) {
+            checkDerivComROWLayout('desktop');
+        }
+    });
+});
