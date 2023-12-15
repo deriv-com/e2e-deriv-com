52b2a440390b4bb6bb10ebbf7aa9caa85701eb36
Mahnoor <mahnoor@Mahnoor-Guls-MacBook-Pro-C02F84G4MD6M-Standard.local>
2023-12-15 14:43:02 +0800

    check-all deriv.com link-part1

---
+++ cypress/e2e/full/checkAllLinks.cy.js
@@ -0,0 +1,54 @@
+import '@testing-library/cypress/add-commands'
+describe('check Staging deriv.com URLs', () => {
+  const capturedUrls = [];
+  it('retrieve all URLs and check for broken links', () => {
+    cy.c_visitResponsive(Cypress.env('RegionROW'), 'desktop')
+    cy.get('.disclaimer-module--risk_warning_container--7005e > .text-small').should('be.visible');
+    cy.get('a').each(($link) => {
+      const url = $link.prop('href');
+      if (url.includes('deriv.com') && !url.includes('.exe')) {
+        capturedUrls.push(url);
+        console.log("Captured URLs:", url);
+        cy.request({
+          url,
+          failOnStatusCode: false,
+        }).then((response) => {
+          if (response.status !== 200) {
+            cy.log(`Broken link found: ${url}`);
+          }
+        });
+      }
+    }).then(() => {
+      cy.log('Captured URLs:');
+      capturedUrls.forEach((url) => {
+        cy.log(url);
+      });
+    });
+  });
+
+  it('visit URLs, Capture Child Links, and Display', () => {
+    // Visit each captured URL, capture child links, and display them
+    capturedUrls.forEach((url, index) => {
+      cy.log(`Visiting URL ${index + 1}/${capturedUrls.length}: ${url}`);
+      cy.visit(url);
+      cy.get('a').each(($link) => {
+        const childUrl = $link.prop('href');
+        if (childUrl.includes('deriv.com') && !childUrl.includes('.pdf')) {
+          cy.request({
+            url: childUrl,
+            failOnStatusCode: false,
+          }).then((response) => {
+            if (response.status !== 200) {
+              cy.log(`Broken link found: ${childUrl}`);
+            }
+            else {
+              cy.log(`Child Link with "staging.deriv": ${childUrl}`);
+              cy.log('childCaptured URLs:');
+            }
+          });
+        }
+      });
+    });
+  });
+});
+
