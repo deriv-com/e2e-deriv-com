3e28b7231e43943d5b89cbfbcc7a20100d91ded1
Mahnoor <mahnoor@Mahnoor-Guls-MacBook-Pro-C02F84G4MD6M-Standard.local>
2023-12-19 18:03:02 +0800

    added exception handler added more checks for childurl

--- cypress/e2e/full/checkAllLinks.cy.js
+++ cypress/e2e/full/checkAllLinks.cy.js
@@ -1,8 +1,7 @@
 import '@testing-library/cypress/add-commands'
-
 describe('check deriv.com URLs', () => {
   const capturedUrls = [];
-  it('retrieve all URLs and check for broken links', () => {
+  it.only('retrieve all URLs and check for broken links', () => {
     cy.c_visitResponsive(Cypress.env('RegionROW'), 'desktop')
     cy.get('a').each(($link) => {
       const url = $link.prop('href');
@@ -12,39 +11,47 @@ describe('check deriv.com URLs', () => {
         cy.request({
           url,
           failOnStatusCode: false,
-
-        }).then((response) => {
-          if (response.status !== 200) {
-            cy.log("Broken link found:"(url));
-          }
-          else if (response.headers['content-type'].includes('application/pdf')) {
-            cy.request({
-              url: url,
-              encoding: 'binary',
-              responseType: 'blob',
-            }).then((pdfResponse) => {
-              expect(pdfResponse.status).to.eq(200);
-              expect(pdfResponse.headers['content-type']).to.include('application/pdf');
-            });
-          }
-        });
+        })
+          .then((response) => {
+            if (response.status !== 200) {
+              cy.log("Broken link found:"(url));
+            }
+            else if (response.headers['content-type'].includes('application/pdf')) {
+              cy.request({
+                url: url,
+                encoding: 'binary',
+                responseType: 'blob',
+              })
+                .then((pdfResponse) => {
+                  expect(pdfResponse.status).to.eq(200);
+                  expect(pdfResponse.headers['content-type']).to.include('application/pdf');
+                });
+            }
+          });
       }
-    }).then(() => {
-      cy.log('Captured URLs:');
-      capturedUrls.forEach((url) => {
-        cy.log(url);
+    })
+      .then(() => {
+        cy.log('Captured URLs:');
+        capturedUrls.forEach((url) => {
+          cy.log(url);
+        });
       });
-    });
   });
 
   it('visit all captured URLs, and capture their child links', () => {
     const uniqueLinks = [...new Set(capturedUrls)];
     console.log(uniqueLinks);
-    const excludedTerms = ['.exe', 'trustpilot', 'blog.deriv.com', 'api.deriv.com', 'community.deriv.com', '.pdf'];
-    const applink = ["app.deriv.com", "smarttrader"]
+    const excludedLinks = ['.exe', 'trustpilot', 'blog.deriv.com', 'api.deriv.com', 'community.deriv.com', '.pdf'];
+    const applink = ["app.deriv.com", "smarttrader.deriv.com", "bot.deriv.com"]
+    cy.on('uncaught:exception', (err, runnable) => {
+      if (err.message.includes("Cannot read properties of null")) {
+        return true;
+      }
+      return false;
+    });
     uniqueLinks.forEach((url, index) => {
       cy.log(`Visiting URL ${index + 1}/${uniqueLinks.length}: ${url}`);
-      if (!excludedTerms.some(term => url.includes(term))) {
+      if (!excludedLinks.some(term => url.includes(term))) {
         if (applink.some(term => url.includes(term))) {
           cy.visit(url);
         }
@@ -53,29 +60,27 @@ describe('check deriv.com URLs', () => {
           cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible');
         }
       }
-
       cy.get('a').each(($link) => {
         const childUrl = $link.prop('href');
-        if (childUrl.includes('deriv.com')) {
+        
+        
+        if (childUrl.includes('deriv.com') && !capturedUrls.includes(childUrl)) {
+    
           cy.request({
             url: childUrl,
             failOnStatusCode: false,
-          }).then((response) => {
-            if (response.status !== 200) {
-              cy.log("Broken link found:", (childUrl));
-            }
-            else {
-              cy.log("Child Link with deriv.com:", (childUrl));
-              console.log('childCaptured URLs:', childUrl);
-            }
-          });
+          })
+            .then((response) => {
+              if (response.status !== 200) {
+                cy.log("Broken link found:", (childUrl));
+              }
+              else {
+                cy.log("Child Link with deriv.com:", (childUrl));
+                console.log('childCaptured URLs:', childUrl);
+              }
+            });
         }
       });
     });
   });
-
-
-
-
-
 });
