ffb5429ade6df31724b09dcd6954e2bc1595e9de
mahnoorgul-deriv <142874666+mahnoorgul-deriv@users.noreply.github.com>
2023-12-28 13:57:43 +0800

    fixed-allHomeHeader (#84)
    * fixed-allHomeHeader
    
    * removed spaces
    
    * removed semicolons and force true

--- cypress/e2e/smoke/allHomeHeader.cy.js
+++ cypress/e2e/smoke/allHomeHeader.cy.js
@@ -1,33 +1,31 @@
 import '@testing-library/cypress/add-commands'
+import homeBanner from '../../support/POM/homePage'
 
 
-function check_homepage_header (){
-    cy.findByRole('img', { name: 'hamburger menu' }).should('be.visible')
-    cy.findByRole('link', { name: 'deriv-logo' })
-      .should('be.visible').click()
+function check_homepage_header() {
+    homeBanner.elements.hamBurgerMenu().should('be.visible')
+    homeBanner.elements.derivLogo().click()
     cy.url().should('include', '/')
-    cy.findByText('EN', { exact: true }).should('be.visible')
 }
 
-function check_hamburger_menu () {
-    cy.findByRole('img', { name: 'hamburger menu' }).should('be.visible').click()
-    cy.findByRole('button', { name: 'Trade chevron' }).should('be.visible')
-    cy.findByRole('button', { name: 'Markets chevron' }).should('be.visible')
-    cy.findByRole('button', { name: 'About us chevron' }).should('be.visible')
-    cy.findByRole('button', { name: 'Resources chevron' }).should('be.visible')
-    cy.findByRole('button', { name: 'Legal chevron' }).should('be.visible')
-    cy.findByRole('button', { name: 'Partners chevron' }).should('be.visible')
-    cy.findByRole('img', { name: 'close menu' }).click()
+function check_hamburger_menu()
+ {
+    homeBanner.elements.hamBurgerMenu().should('be.visible').click()
+    homeBanner.elements.tradeMenu().should('be.visible')
+    homeBanner.elements.marketsMenu().should('be.visible')
+    homeBanner.elements.aboutUsMenu().should('be.visible')
+    homeBanner.elements.resourcesMenu().should('be.visible')
+    homeBanner.elements.legalMenu().should('be.visible')
+    homeBanner.elements.partnersMenu().should('be.visible')
+    homeBanner.elements.crossIcon().click()
 }
-
-
 
 describe('QATEST-1298 - should validate home page header menu', () => {
     const regions = ['ROW', 'EU'];
 
     regions.forEach((region) => {
         it(`should be able validate homepage header for ${region} website`, () => {
-            cy.c_visitResponsive(`/${Cypress.env('Region' + region)}`);
+            cy.c_visitResponsive(`/${Cypress.env('Region' + region)}`)
             check_homepage_header()
             check_hamburger_menu()
             cy.findByRole('button', { name: 'Log in' }).should('be.visible')
--- cypress/support/POM/homePage.js
+++ cypress/support/POM/homePage.js
@@ -6,8 +6,23 @@ class homeBanner {
         commoditiesTxt: () => cy.findByTestId('typewriter-wrapper').findByText('Commodities'),
         derivedIndicesTxt: () => cy.findByTestId('typewriter-wrapper').findByText('Derived Indices'),
         createFreeDemoAccount: () => cy.findByLabelText('create free demo account'),
+        hamBurgerMenu: () => cy.get('[data-cy="hamburger-menu"]').eq(1),
+        derivLogo: () => cy.findByRole('navigation'),
+        langEnglish: () => cy.findByText('English'),
+        tradeMenu: () => cy.findByRole('list').contains('Trade'),
+        marketsMenu: () => cy.findByRole('list').contains('Markets'),
+        aboutUsMenu: () => cy.findByRole('list').contains('About us'),
+        resourcesMenu: () => cy.findByRole('list').contains('Resources'),
+        legalMenu: () => cy.findByRole('list').contains('Legal'),
+        partnersMenu: () => cy.findByRole('list').contains('Partners'),
+        crossIcon: () => cy.get('[class="cursor-pointer"]'),   
+        cfdMenu: () =>  findByRole('link', { name: 'CFDs' }),
+        optionsMenu: () =>  findByRole('link', { name: 'Options' }),
+        multiplierMenu: () =>  findByRole('link', { name: 'Multipliers' }),
+
 
     }
+
 }
 
 module.exports = new homeBanner();
