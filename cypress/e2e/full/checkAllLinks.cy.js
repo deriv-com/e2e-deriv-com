3fd0e3c45d28be28b76726a140012f521091ed6b
Mahnoor <mahnoor@regentmarkets.com>
2023-12-28 09:51:52 +0800

    updated script

--- cypress/e2e/full/checkAllLinks.cy.js
+++ cypress/e2e/full/checkAllLinks.cy.js
@@ -35,18 +35,18 @@ function checkAndLogBrokenLinks(derivLinks) {
 }
 
 describe('check deriv.com URLs', () => {
-  it('capture all home page urls, and then visit caotured urls and their child links ', () => {
+  it('capture all home page urls, and then visit catured urls and their child links ', () => {
     checkAndLogBrokenLinks(derivLinks);
     cy.wrap(capturedUrls).should('not.be.empty').then(() => {
-      console.log("captured", capturedUrls);
+
       const uniqueLinks = [...new Set(capturedUrls)];
       console.log(uniqueLinks);
       console.log("captured", capturedUrls);
       cy.c_visitResponsive(Cypress.env('RegionROW'), 'desktop');
-      const excludedLinks = ['.exe' ,'trustpilot', 'blog.deriv.com', 'api.deriv.com', 'community.deriv.com', '.pdf', "twitter", "www."];
-      const applink = ["app.deriv.com", "smarttrader.deriv.com", "bot.deriv.com", "p2p"];
+      const excludedLinks = ['.exe', 'trustpilot', 'blog.deriv.com', 'api.deriv.com', 'community.deriv.com', '.pdf', "twitter", "www."];
+      const applink = ["app.deriv.com", "smarttrader.deriv.com", "bot.deriv.com"];
       cy.on('uncaught:exception', (err, runnable) => {
-        if (err.message.includes("Cannot read properties of null")) {
+        if (err.message.includes('Uncaught TypeError: Cannot read properties of null')) {
           return true;
         }
         return false;
@@ -54,9 +54,10 @@ describe('check deriv.com URLs', () => {
       uniqueLinks.forEach((url, index) => {
         cy.log(`Visiting URL ${index + 1}/${uniqueLinks.length}: ${url}`);
         if (!excludedLinks.some(term => url.includes(term))) {
-          if (applink.some(term => url.includes(term))) {
+          if  (applink.some(term => url.includes(term))) {
             cy.visit(url);
-          } else {
+          }
+           else {
             cy.visit(url);
             cy.findByRole('button', { name: 'whatsapp icon' }).should('be.visible');
             cy.findByRole('heading', { name: 'The requested URL was not found' }).should('not.exist');
@@ -70,7 +71,7 @@ describe('check deriv.com URLs', () => {
         cy.get('a').each(($link) => {
           const childUrl = $link.prop('href');
           if (containsDerivSubstring(childUrl) && !capturedUrls.includes(childUrl)) {
-            allChildUrls.push(childUrl); 
+            allChildUrls.push(childUrl);
             cy.request({
               url: childUrl,
               failOnStatusCode: false,
@@ -80,12 +81,14 @@ describe('check deriv.com URLs', () => {
               } else {
                 cy.log("Child Links with deriv:", childUrl);
                 console.log('childCaptured URLs:', childUrl);
-                
+
               }
             });
           }
         });
+          });
+        });
       });
     });
-  });
-});
+      
+    
