346c92100ad41dac6b35672f9d9f3184ae86a7ad
Pravin Kannappan <pravin@regentmarkets.com>
2024-01-17 15:37:37 +0800

    add baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -10,7 +10,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) { },
     // baseUrl: 'https://deriv.com',
     //baseUrl: 'https://deriv-com-v2.pages.dev/',
-    baseUrl: 'https://deriv-com-git-translation-new-homepage.binary.sx/', 
+    baseUrl: 'https://staging.deriv.com/', 
     defaultCommandTimeout: 10000,
     supportFile: "cypress/support/e2e.js",
     
