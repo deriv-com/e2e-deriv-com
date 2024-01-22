8e27f4bd98f9c9fedb540dadfe8298855fd7bae0
Habib Deriv <88178645+habib-deriv@users.noreply.github.com>
2024-01-22 18:09:08 +0800

    Revert "Fixed Smoke test failure"

--- cypress/support/POM/commonPage.js
+++ cypress/support/POM/commonPage.js
@@ -55,14 +55,13 @@ class footer {
   }
 
   areSocialLinksCorrect(socialLink, socialWebsiteUrl) {
-    socialLink().click({force: true})
-    
+    socialLink().click()
     this.elements.alertDialog().should('be.visible')
     this.elements.alertRedirectMessage().should('be.visible')
     cy.window().then((win) => {
       cy.stub(win, 'open').as('windowOpen')
     })
-    this.elements.proceedButton().click({force: true})
+    this.elements.proceedButton().click()
     cy.reload()
     cy.get('@windowOpen').should('be.calledWith', socialWebsiteUrl)
   }
