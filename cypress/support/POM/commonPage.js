c5c7746728b6ab87de339aee56f6662c11b37bbb
Bilal Bin Majid <155507124+bilalmajid-deriv@users.noreply.github.com>
2024-01-23 15:32:54 +0800

    Added wait for whatsapp icon after cy.reload() so that the page is completely loaded. (#100)

--- cypress/support/POM/commonPage.js
+++ cypress/support/POM/commonPage.js
@@ -23,6 +23,8 @@ class footer {
         .findByText('You are being redirected to an external website.'),
     cfdFloatingBannerLink: () =>
       cy.contains('% of retail investor accounts lose money when trading CFDs with Deriv, read our full Risk disclosure here'),
+    whatsappIcon: () =>
+    cy.findByRole("button", { name: "whatsapp icon" }),
   }
 
   riskDisclosurePdf = {
@@ -63,6 +65,7 @@ class footer {
     })
     this.elements.proceedButton().click()
     cy.reload()
+    this.elements.whatsappIcon().should("be.visible", {timeout: 30000});
     cy.get('@windowOpen').should('be.calledWith', socialWebsiteUrl)
   }
 }
