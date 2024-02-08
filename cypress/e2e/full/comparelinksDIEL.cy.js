2417a2aef3842565f64773f509d2dfe26d350324
maliheh <maliheh@regentmarkets.com>
2024-02-08 15:32:06 +0800

    wip

--- cypress/e2e/full/comparelinksDIEL.cy.js
+++ cypress/e2e/full/comparelinksDIEL.cy.js
@@ -1,102 +1,87 @@
 import '@testing-library/cypress/add-commands'
-import { listenerCount } from 'process';
-  // returning false here prevents Cypress from failing the test on uncaught:exception
+import { allLinks } from '../../support/POM/expectedURLs'
+// returning false here prevents Cypress from failing the test on uncaught:exception
 Cypress.on('uncaught:exception', (err, runnable) => {
   return false
 })
 
 describe('QQAA-1178 - Compare Links', () => {
-    it('visits all links for DIEL residance and compare links with pre-defined list', () => {
-      // Visit homepage
-      // cy.c_visitResponsive(Cypress.env('RegionDIEL'), 'desktop');
-        let List1 =  [
-          'https://community.deriv.com/',
-          'https://staging.deriv.com/',
-          'https://avc.deriv.com/',
-          'https://dd.deriv.com/',
-          'https://d4.deriv.com/',
+  it('visits all links for DIEL residance and compare links with pre-defined list', () => {
+    // Visit homepage
+    // cy.c_visitResponsive(Cypress.env('RegionDIEL'), 'desktop');
+    if (allLinks.CurrentLinks.length == allLinks.ExpectedLinks_DIEL.length) {
+      cy.log('Lengths are equal.');
+      try {
+        // Check if allLinks.CurrentLinks have all links of allLinks.ExpectedLinks_DIEL
+        expect(allLinks.CurrentLinks).to.have.members(allLinks.ExpectedLinks_DIEL);
+        cy.log('Logs identical, test passed');
+      }
+      catch (error) {
+        // If the expectation failed , continue with following checks : 
+        allLinks.MissingLinks = allLinks.CurrentLinks.filter(link => !allLinks.ExpectedLinks_DIEL.includes(link));
+        allLinks.NewLinks = allLinks.ExpectedLinks_DIEL.filter(link => !allLinks.CurrentLinks.includes(link));
+        cy.log('Please check the missing links:');
+        cy.log(allLinks.MissingLinks);
+        cy.log('Please check the New links:');
+        cy.log(allLinks.NewLinks);
+      }
+    }
+    if (allLinks.CurrentLinks.length > allLinks.ExpectedLinks_DIEL.length) {
+      cy.log('allLinks.CurrentLinks length is :');
+      cy.log(allLinks.CurrentLinks.length);
+      cy.log('allLinks.ExpectedLinks_DIEL length is :');
+      cy.log(allLinks.ExpectedLinks_DIEL.length);
+      try {
+        expect(allLinks.CurrentLinks).to.include.members(allLinks.ExpectedLinks_DIEL);
+        allLinks.MissingLinks = allLinks.CurrentLinks.filter(link => !allLinks.ExpectedLinks_DIEL.includes(link));
+        cy.log('Please check the Missing links:');
+        cy.log(allLinks.MissingLinks);
+      }
+      catch (error) {
+        allLinks.MissingLinks = allLinks.CurrentLinks.filter(link => !allLinks.ExpectedLinks_DIEL.includes(link));
+        allLinks.NewLinks = allLinks.ExpectedLinks_DIEL.filter(link => !allLinks.CurrentLinks.includes(link));
+        cy.log('Please check the missing links:');
+        cy.log(allLinks.MissingLinks);
+        cy.log('Please check the New links:');
+        cy.log(allLinks.NewLinks);
+      }
+    }
+    if (allLinks.CurrentLinks.length < allLinks.ExpectedLinks_DIEL.length) {
+      cy.log('allLinks.CurrentLinks length is :');
+      cy.log(allLinks.CurrentLinks.length);
+      cy.log('allLinks.ExpectedLinks_DIEL length is :');
+      cy.log(allLinks.ExpectedLinks_DIEL.length);
+      try {
+        expect(allLinks.ExpectedLinks_DIEL).to.include.members(allLinks.CurrentLinks);
+        allLinks.NewLinks = allLinks.ExpectedLinks_DIEL.filter(link => !allLinks.CurrentLinks.includes(link));
+        cy.log('Please check the New links:');
+        cy.log(allLinks.NewLinks);
+      }
+      catch (error) {
+        allLinks.MissingLinks = allLinks.CurrentLinks.filter(link => !allLinks.ExpectedLinks_DIEL.includes(link));
+        allLinks.NewLinks = allLinks.ExpectedLinks_DIEL.filter(link => !allLinks.CurrentLinks.includes(link));
+        cy.log('Please check the missing links:');
+        cy.log(allLinks.MissingLinks);
+        cy.log('Please check the New links:');
+        cy.log(allLinks.NewLinks);
+      }
+    }
+    cy.wrap(allLinks.MissingLinks,{log:false}).then((list)=>{
+      expect(list.length).to.eq(0);
+    })
+    cy.wrap(allLinks.NewLinks,{log:false}).then((list)=>{
+      expect(list.length).to.eq(0);
+    })
 
-        ];
-        let List2 = [
-          'https://staging.deriv.com/',
-          'https://community.deriv.com/',
-          'https://avc.deriv.com/',
-          // 'https://dd.deriv.com/',
-        
-           
-        ];
-          let MissingLinks = [];
-          let NewLinks = [];
 
-          if (List1.length === List2.length) {
-          cy.log('Lengths are equal.');
-          try {
-            // Check if List1 have all links of List2
-            expect(List1).to.have.members(List2);
-            cy.log('Logs identical, test passed');
-          }
-           catch (error) {
-            // If the expectation failed , continue with following checks : 
-          const MissingLinks = List1.filter(link => !List2.includes(link));
-          const NewLinks = List2.filter(link => !List1.includes(link));
-          cy.log('Please check the missing links:');
-          cy.log(MissingLinks);
-          cy.log('Please check the New links:');
-          cy.log(NewLinks);
-          }}
-          if (List1.length > List2.length) {
-            cy.log('List1 length is :'); 
-            cy.log (List1.length);
-            cy.log('List2 length is :');
-            cy.log (List2.length); 
-              try {
-          expect(List1).to.include.members(List2);
-          const MissingLinks = List1.filter(link => !List2.includes(link));
-          cy.log('Please check the Missing links:');
-          cy.log(MissingLinks);
-                }
-              catch (error) {
-              const MissingLinks = List1.filter(link => !List2.includes(link));
-              const NewLinks = List2.filter(link => !List1.includes(link));
-              cy.log('Please check the missing links:');
-              cy.log(MissingLinks);
-              cy.log('Please check the New links:');
-              cy.log(NewLinks);
-              }
-              expect(NewLinks).to.be.empty;
-              expect(MissingLinks).to.be.empty;
-            }
-           if (List1.length < List2.length) {
-            cy.log('List1 length is :'); 
-            cy.log (List1.length);
-            cy.log('List2 length is :');
-            cy.log (List2.length);
-              try {
-          expect(List2).to.include.members(List1);
-          const NewLinks = List2.filter(link => !List1.includes(link));
-          cy.log('Please check the New links:');
-          cy.log(NewLinks);
-                }
-              catch (error) {
-              const MissingLinks = List1.filter(link => !List2.includes(link));
-              const NewLinks = List2.filter(link => !List1.includes(link));
-              cy.log('Please check the missing links:');
-              cy.log(MissingLinks);
-              cy.log('Please check the New links:');
-              cy.log(NewLinks);
-              }
-              expect(NewLinks).to.be.empty;
-              expect(MissingLinks).to.be.empty;
-            }
-           
-          });
-           
-        });
-        
-      
+  });
 
-    
-    
-     
+});
+
+
+
+
+
+
 
 
---
+++ cypress/support/POM/expectedURLs.js
@@ -0,0 +1,15 @@
+export class allLinks {
+    static MissingLinks = [];
+    static NewLinks = [];
+    static CurrentLinks = [
+        'https://avc.deriv.com/',
+        'https://dd.deriv.com/',
+    ];
+    static ExpectedLinks_DIEL = [
+      'https://avc.deriv.com/',
+      'https://dd.deriv.com/',
+    ];
+  }
+
+
+
