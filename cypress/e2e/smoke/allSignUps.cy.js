05bf177ed3fd5d71d6e269e718afe0daf4fc8d46
pravin-deriv <103259298+pravin-deriv@users.noreply.github.com>
2024-02-28 00:30:39 +0800

    add coverage for signup (#133)

--- cypress/e2e/smoke/allSignUps.cy.js
+++ cypress/e2e/smoke/allSignUps.cy.js
@@ -15,14 +15,25 @@ describe('QATEST-1378 - should validate signup page', () => {
     signUpPage.elements.logInTxt().should('be.visible')
   }
 
-  it('should be able to sign up on a ROW website', () => {
+  it('should be able to sign up on a ROW mobile', () => {
     cy.c_visitResponsive('/signup' + String(Cypress.env('RegionROW')))
     validateSignUpPage()
   })
 
-  it('should be able to sign up on a EU website', () => {
+  it('should be able to sign up on a EU mobile', () => {
     cy.c_visitResponsive('/signup' + String(Cypress.env('RegionEU')))
     validateSignUpPage()
-    footer.elements.cfdFloatingBannerLink().should('be.visible')
+    footer.elements.cfdFloatingBannerLink().should('exist')
+  })
+
+  it('should be able to sign up on a ROW desktop', () => {
+    cy.c_visitResponsive('/signup/' + Cypress.env('RegionROW'), 'desktop');
+    validateSignUpPage()
+  })
+
+  it('should be able to sign up on a EU desktop', () => {
+    cy.c_visitResponsive('/signup/' + Cypress.env('RegionEU'), 'desktop');
+    validateSignUpPage()
+    footer.elements.cfdFloatingBannerLink().should('exist')
   })
 })
