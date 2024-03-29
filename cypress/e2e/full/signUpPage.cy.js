e46c822e22eef95363dd09da74241239774acd76
Alena-deriv <153058266+Alena-deriv@users.noreply.github.com>
2024-03-29 11:30:29 +0300

    DPROD-3901 (#171)
    * commit_message
    
    * add correction
    
    * fix comment
    
    * fix comment on test structure
    
    ---------
    
    Co-authored-by: Alena Sachylovich <alenasachylovich@Alena-Sachylovichs-MacBook-Pro-C02F4AQJMD6M-Standard.local>

---
+++ cypress/e2e/full/signUpPage.cy.js
@@ -0,0 +1,69 @@
+import '@testing-library/cypress/add-commands'
+
+function checkSignUpPage () {
+    cy.findByRole('heading', { name: 'Join over 2.5 million traders' }).should('be.visible')
+    cy.findByPlaceholderText('Email').should('be.visible')
+    cy.findByRole('checkbox').should('be.visible')
+    cy.findByRole('link', { name: 'terms and conditions' }).should('be.visible')
+    cy.findByRole('button', { name: 'Create demo account' }).should('be.visible')
+    cy.findByRole('link', { name: 'Security and privacy policy' }).should('be.visible')
+    cy.findByRole('button', { name: 'Google' }).should('be.visible')
+    cy.findByRole('button', { name: 'Facebook' }).should('be.visible')
+    cy.findByRole('button', { name: 'Apple' }).should('be.visible')
+    cy.findByText('Log in').should('be.visible') 
+}
+
+function enterEmail(email)
+{
+    cy.findByPlaceholderText('Email').type(email)
+    cy.findByRole('checkbox').click()
+    cy.findByRole('button', { name: 'Create demo account' }). click ()
+}
+
+function verificationIsSent () {
+    cy.findByRole('heading', { name: 'Check your email' }).should('be.visible')
+    cy.contains ('We\'ve sent a message to').should('be.visible')
+    cy.findByRole('link', { name: 'Didn\'t receive your email?' }).should('be.visible')
+}
+
+describe('QATEST-1613 - should validate the Sign Up page on desktop', () => {
+
+    it('should be able to navigate to Sign Up page for ROW', () => {
+        cy.c_visitResponsive(`signup/${Cypress.env('RegionROW')}`,'desktop')
+        checkSignUpPage ()
+        cy.c_generateRandomEmail('@test.com').then((email) => {
+        enterEmail(email)
+        verificationIsSent ()
+    })
+
+    it('should be able to navigate to Sign Up page for EU', () => {
+        cy.c_visitResponsive(`signup/${Cypress.env('RegionEU')}`,'desktop')
+        checkSignUpPage ()
+        cy.c_generateRandomEmail('@test.com').then((email) => {
+        enterEmail(email)
+        verificationIsSent ()
+    })
+})
+})
+})
+
+describe('QATEST-1613 - should validate the Sign Up page on mobile', () => {
+
+    it('should be able to navigate to Sign Up page for ROW', () => {
+        cy.c_visitResponsive(`signup/${Cypress.env('RegionROW')}`)
+        checkSignUpPage ()
+        cy.c_generateRandomEmail('@test.com').then((email) => {
+        enterEmail(email)
+        verificationIsSent ()
+        })
+    })
+})
+
+    it('should be able to navigate to Sign Up page for EU', () => {
+        cy.c_visitResponsive(`signup/${Cypress.env('RegionEU')}`)
+        checkSignUpPage ()
+        cy.c_generateRandomEmail('@test.com').then((email) => {
+        enterEmail(email)
+        verificationIsSent ()
+    })
+})
