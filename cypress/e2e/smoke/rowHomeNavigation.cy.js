898b6398ca9fac4292b82e61a093e707618b01be
hasan-deriv <abuhasan@deriv.com>
2024-04-01 16:30:16 +0800

    chore: updated row home navigation links

--- cypress/e2e/smoke/rowHomeNavigation.cy.js
+++ cypress/e2e/smoke/rowHomeNavigation.cy.js
@@ -2,10 +2,10 @@ import '@testing-library/cypress/add-commands'
 
 describe('QATEST-1266 - deriv.com ROW home page should be opened with the url as same as deriv.com', () => {
     const checkDerivComROWLayout = (layout) => {
-        cy.c_visitResponsive(Cypress.env('RegionROW'), layout , true) 
+        cy.c_visitResponsive('', layout , true) 
         const baseUrl = Cypress.config('baseUrl').toLowerCase();
         const urlToMatch = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl
-        cy.url().should('eq', urlToMatch + Cypress.env('RegionROW'))
+        cy.url().should('eq', urlToMatch)
         cy.findByText('Trade CFDs and options on global financial markets, all in one place with 24/7 trading and worldwide support.').should('be.visible')
     }
 
