806877fac90a880da9a6d5648c4aab1a97b54c00
hasan-deriv <abuhasan@deriv.com>
2024-04-01 16:22:18 +0800

    chore: updated row footer links

--- cypress/e2e/smoke/rowFooter.cy.js
+++ cypress/e2e/smoke/rowFooter.cy.js
@@ -9,7 +9,7 @@ import responsiblePage from '../../support/POM/responsiblePage'
 
 describe('QATEST-1399 - Footer ROW Responsive', () => {
   beforeEach(() => {
-    cy.c_visitResponsive(Cypress.env('RegionROW'), 'small')
+    cy.c_visitResponsive('', 'small')
   })
   const externalSocialUrls = Cypress.config('externalSocialUrls')
   const { googlePlay, appStore, appGallery } = Cypress.config().appMarketUrl;
