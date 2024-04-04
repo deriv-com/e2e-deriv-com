7405bd5d7b6158ac16fc5f6ac8efd92bfb739c17
mayuran-deriv-dev96 <balamayuran@regentmarkets.com>
2024-04-04 13:18:59 +0400

    fix: pr comment

--- cypress/e2e/smoke/euHomeHamburger.cy.js
+++ cypress/e2e/smoke/euHomeHamburger.cy.js
@@ -212,7 +212,10 @@ describe('QATEST-1274 - Navigation Responsive - Open/Close Menu', () => {
     homeBanner.elements.crossIcon().click()
     cy.findByText('Cookies help us to give you a better experience and personalised content on our site.').should('be.visible')
     cy.contains("Don't accept").should('be.visible').click()
-    cy.findByTestId("warning-pop-up").then(($warningPopUp) => $warningPopUp.length > 0 && cy.findByTestId("warning-pop-up-close-button").click())
+    cy.document().then(doc => {
+      const popup = doc.querySelector('[data-testid="warning-pop-up"]')
+      if (popup) { cy.findByTestId("warning-pop-up-close-button").click() }
+  }) 
     const middle = Cypress.$(cy.state('window')).height() / 2
     const scrollOffset = 400
     cy.scrollTo(0, middle + scrollOffset)
