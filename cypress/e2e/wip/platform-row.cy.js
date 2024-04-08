86f5c8fb52f4b8a77a2f8088c48d3cd6e4a5ce8b
shenbakavanee <shenbakavanee@regentmarkets.com>
2024-04-08 14:56:08 +0800

    address review comment part 1

--- cypress/e2e/wip/platform-row.cy.js
+++ cypress/e2e/wip/platform-row.cy.js
@@ -7,32 +7,32 @@ describe('QATEST-1350 - Our Platform ROW', () =>
         cy.findByText("User-friendly trading platforms, on any device").click()
         cy.findByText("All").click();
         
-        // Check CFDs tab
-        cy.get('#platformtab-trigger-1').click();
-        // Check Deriv MT5 
+        // //Check CFDs tab
+        cy.findByRole('tab', { name: 'CFDs' })
+        // // Check Deriv MT5 
         cy.findByRole("heading", { name: "Deriv MT5" }).should('be.visible');
-        cy.get('img[alt="deriv mt5"]').should('be.visible').click();
+        cy.findAllByText('Deriv MT5').eq(0).should('be.visible').click()
         cy.findByText("The most popular and comprehensive CFDs platform.").should('be.visible')
         cy.findByRole('link', { name: 'Learn more About Deriv MT5' }).click();
-        cy.get('img[alt="Deriv MT5"]').first().click();
+        cy.findByText("Why trade with Deriv MT5").should('be.visible')
         cy.findByRole('link', { name: 'deriv logo label' }).click(); 
-        // Check Ctrader
+        //Check Ctrader
         cy.findByRole("heading", { name: "Deriv cTrader" }).should('be.visible');
-        cy.get('img[alt="deriv ctrader"]').should('be.visible').click();
+        cy.get('img[alt="deriv ctrader"]').should('be.visible')//need to update later
         cy.findByText("Fast CFDs platform with inbuilt copy trading.").should('be.visible')
         cy.findByRole('link', { name: 'Learn more About Deriv cTrader' }).click();
         cy.findByRole('img', { name: '_t_Deriv ctrader logo_t_' }).click();
         cy.findByRole('link', { name: 'deriv logo label' }).click(); 
         //Check Deriv X
         cy.findByRole("heading", { name: "Deriv X" }).should('be.visible');
-        cy.get('img[alt="deriv x"]').should('be.visible').click();
+        cy.get('img[alt="deriv x"]').should('be.visible').click(); //need to update later
         cy.findByText("User-friendly and customisable CFDs platform.").should('be.visible')
         cy.findByRole('link', { name: 'Learn more About Deriv X' }).click();
-        cy.get('img[alt="Deriv X"]').first().click();
+        cy.get('img[alt="Deriv X"]').first().click(); //need to update later
         cy.findByRole('link', { name: 'deriv logo label' }).click(); 
 
         // Check Options tab
-        cy.get('#platformtab-trigger-2').click();
+        cy.contains('[role="tab"][id="platformtab-trigger-2"]', 'Options').click();
         // Find an element by its label containing "Options"
         cy.findByLabelText("Options").within(() => {
             cy.findByRole("heading", { name: "Deriv GO" }).should('be.visible');
@@ -51,29 +51,29 @@ describe('QATEST-1350 - Our Platform ROW', () =>
         cy.findByRole('link', { name: 'deriv logo label' }).click(); 
         //Check Dtrader
         cy.findByRole('link', { name: 'Learn more About Deriv Trader' }).click();
-        cy.findByRole('img', { name: 'Deriv Trader' }).click();
+        cy.findByRole('img', { name: 'Deriv Trader' }).should('be.visible')
         cy.findByRole('link', { name: 'deriv logo label' }).click(); 
         //Check Bot
         cy.findByRole('link', { name: 'Learn more About Deriv Bot' }).click();
-        cy.findByRole('img', { name: 'Deriv Bot' }).click();
+        cy.findByRole('img', { name: 'Deriv Bot' }).should('be.visible')
         cy.findByRole('link', { name: 'deriv logo label' }).click(); 
 
         // Check Bot tab
-        cy.get('#platformtab-trigger-3').click();
+        cy.findByRole('tab', { name: 'Bots' })
         cy.findByRole("heading", { name: "Deriv Bot" }).should('be.visible');
         cy.get('img[alt="deriv bot"]').should('be.visible').click();
         cy.findByText("Automated trading. No coding required.").should('be.visible')
         cy.findByRole('link', { name: 'Learn more About Deriv Bot' }).click();
-        cy.findByRole('img', { name: 'Deriv Bot' }).click();
+        cy.findByRole('img', { name: 'Deriv Bot' }).should('be.visible');
         cy.findByRole('link', { name: 'deriv logo label' }).click(); 
         
         // Check Social tab
-        cy.get('#platformtab-trigger-4').click();
+        cy.findByRole('tab', { name: 'Social' })
         cy.findByRole("heading", { name: "Deriv cTrader" }).should('be.visible');
         cy.get('img[alt="deriv ctrader"]').should('be.visible').click();
         cy.findByText("Fast CFDs platform with inbuilt copy trading.").should('be.visible')
         cy.findByRole('link', { name: 'Learn more About Deriv cTrader' }).click();
-        cy.findByRole('img', { name: '_t_Deriv ctrader logo_t_' }).click();
+        cy.findByRole('img', { name: '_t_Deriv ctrader logo_t_' }).should('be.visible')
         cy.findByRole('link', { name: 'deriv logo label' }).click(); 
 })
 })
