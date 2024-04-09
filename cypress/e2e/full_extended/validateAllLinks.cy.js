ca2a4c9d2fc6d6eebb1bffd9932fff02dbf93a0a
Bilal Bin Majid <155507124+bilalmajid-deriv@users.noreply.github.com>
2024-04-09 12:08:24 +0800

    Bilal/apply prettier formating (#188)
    * Added Prettier formating and check
    
    * removed ;
    
    * added script in package.json
    
    * removed ;
    
    * updated prettier check yml

--- cypress/e2e/full_extended/validateAllLinks.cy.js
+++ cypress/e2e/full_extended/validateAllLinks.cy.js
@@ -1,70 +1,67 @@
-import "@testing-library/cypress/add-commands";
+import '@testing-library/cypress/add-commands'
 
-Cypress.on("uncaught:exception", (err, runnable) => {
+Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test on uncaught:exception
-  return false;
+  return false
 })
 
 let linkDetails = {
-  validDomains: [
-    Cypress.config("baseUrl"),
-    Cypress.env('RegionEU')
-],
+  validDomains: [Cypress.config('baseUrl'), Cypress.env('RegionEU')],
   excludedVisitLinks: [
-    ".exe",
-    ".pdf",
-    "download.mql5.com",
-    "www.metatrader5.com",
-    "play.google.com",
-    "apps.apple.com",
-    "metatraderweb.app",
-    "javascript:;",
-    "zohorecruit",
-    "portal.productboard.com",
-    "goo.gl",
-    "facebook",
-    "twitter",
-    "instagram",
-    "youtube",
-    "https://staging.deriv.com/trade-types/options/", // Bug raised as https://app.clickup.com/t/86bxbkfqm  will remove once the issue is fixed because till then test will keep on failing without verifying other links
-    "https://staging.eu-deriv-com-pages.pages.dev/trade-types/options/", // Bug raised as https://app.clickup.com/t/86bxbkfqm  will remove once the issue is fixed because till then test will keep on failing without verifying other links
-    "https://deriv.com/trade-types/options/",
+    '.exe',
+    '.pdf',
+    'download.mql5.com',
+    'www.metatrader5.com',
+    'play.google.com',
+    'apps.apple.com',
+    'metatraderweb.app',
+    'javascript:;',
+    'zohorecruit',
+    'portal.productboard.com',
+    'goo.gl',
+    'facebook',
+    'twitter',
+    'instagram',
+    'youtube',
+    'https://staging.deriv.com/trade-types/options/', // Bug raised as https://app.clickup.com/t/86bxbkfqm  will remove once the issue is fixed because till then test will keep on failing without verifying other links
+    'https://staging.eu-deriv-com-pages.pages.dev/trade-types/options/', // Bug raised as https://app.clickup.com/t/86bxbkfqm  will remove once the issue is fixed because till then test will keep on failing without verifying other links
+    'https://deriv.com/trade-types/options/',
   ],
   excludedCheckLinks: [
-    "https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/MetaTrader5.dmg", //causing hang because takes a lot of time downloading pdf
-    "https://www.instagram.com/derivcareers/",
-    "javascript:;",
-    "mailto:",
+    'https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/MetaTrader5.dmg', //causing hang because takes a lot of time downloading pdf
+    'https://www.instagram.com/derivcareers/',
+    'javascript:;',
+    'mailto:',
   ],
   linksAllowedFailingVisits: [],
   linksAllowedFailingStatuses: {
     404: [
-      "https://apps.apple.com/us/app/deriv-x/id1563337503",
-      "https://apps.apple.com/us/app/deriv-dp2p/id1506901451",
-      "https://staging.deriv.com/trade-types/options/", // Bug raised as https://app.clickup.com/t/86bxbkfqm  will remove once the issue is fixed because till then test will keep on failing without verifying other links
-      "https://staging.eu-deriv-com-pages.pages.dev/trade-types/options/", // Bug raised as https://app.clickup.com/t/86bxbkfqm  will remove once the issue is fixed because till then test will keep on failing without verifying other links"
+      'https://apps.apple.com/us/app/deriv-x/id1563337503',
+      'https://apps.apple.com/us/app/deriv-dp2p/id1506901451',
+      'https://staging.deriv.com/trade-types/options/', // Bug raised as https://app.clickup.com/t/86bxbkfqm  will remove once the issue is fixed because till then test will keep on failing without verifying other links
+      'https://staging.eu-deriv-com-pages.pages.dev/trade-types/options/', // Bug raised as https://app.clickup.com/t/86bxbkfqm  will remove once the issue is fixed because till then test will keep on failing without verifying other links"
     ],
     999: [
-      "https://www.linkedin.com/company/derivdotcom/",
-      "https://www.linkedin.com/company/derivdotcom/life/",
-      "https://www.linkedin.com/company/derivdotcom/life/",
+      'https://www.linkedin.com/company/derivdotcom/',
+      'https://www.linkedin.com/company/derivdotcom/life/',
+      'https://www.linkedin.com/company/derivdotcom/life/',
     ],
-    429: ["https://blog.deriv.com/"],
-    403: ["https://ark-funds.com/funds/arkk/"],
+    429: ['https://blog.deriv.com/'],
+    403: ['https://ark-funds.com/funds/arkk/'],
   },
   checkedLinks: [], // To check if link has already been checked or not.
   visitedLinks: [], // To check if link has already been visited or not.
-};
+}
 
 let failedLinks = {
   failedCheckLinks: [],
   failedVisitLinks: [],
-};
+}
 
-let passingStatusCodes = [200, 204];
+let passingStatusCodes = [200, 204]
 
-const normalizeUrl = (url) => url.toLowerCase().trim().replace(/\/$/, "")
+const normalizeUrl = (url) => url.toLowerCase().trim().replace(/\/$/, '')
 
 /**
  *
@@ -78,11 +75,11 @@ const isLinkValid = (currentLink) => {
       normalizeUrl(currentLink).includes(normalizeUrl(link))
     )
   ) {
-    return true;
+    return true
   } else {
-    return false;
+    return false
   }
-};
+}
 
 /**
  *
@@ -97,28 +94,28 @@ const isLinkStaging = (type, currentLink) => {
     linkDetails.validDomains[1],
     linkDetails.validDomains[0]
   )
-  if (type == "Visited") {
+  if (type == 'Visited') {
     if (
       linkDetails.visitedLinks.some(
         (link) => normalizeUrl(currentLinkInStaging) == normalizeUrl(link)
       )
     ) {
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
-  } else if (type == "Checked") {
+  } else if (type == 'Checked') {
     if (
       linkDetails.checkedLinks.some(
         (link) => normalizeUrl(currentLinkInStaging) == normalizeUrl(link)
       )
     ) {
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
   }
-};
+}
 
 /**
  *
@@ -129,7 +126,7 @@ const isLinkStaging = (type, currentLink) => {
  * @example isLinkExcluded('Visit', "https://abc.com/")
  */
 const isLinkExcluded = (type, currentLink) => {
-  if (type == "Visit") {
+  if (type == 'Visit') {
     if (
       linkDetails.excludedVisitLinks.some((link) =>
         normalizeUrl(currentLink).includes(normalizeUrl(link))
@@ -137,13 +134,13 @@ const isLinkExcluded = (type, currentLink) => {
       linkDetails.visitedLinks.some(
         (link) => normalizeUrl(currentLink) == normalizeUrl(link)
       ) ||
-      isLinkStaging("Visited", currentLink)
+      isLinkStaging('Visited', currentLink)
     ) {
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
-  } else if (type == "Check") {
+  } else if (type == 'Check') {
     if (
       linkDetails.excludedCheckLinks.some((link) =>
         normalizeUrl(currentLink).includes(normalizeUrl(link))
@@ -151,14 +148,14 @@ const isLinkExcluded = (type, currentLink) => {
       linkDetails.checkedLinks.some(
         (link) => normalizeUrl(currentLink) == normalizeUrl(link)
       ) ||
-      isLinkStaging("Checked", currentLink)
+      isLinkStaging('Checked', currentLink)
     ) {
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
   }
-};
+}
 
 /**
  *
@@ -174,7 +171,7 @@ const isAllowedFailingStatus = (currentLink) => {
       })
     }
   )
-};
+}
 
 /**
  * @param {*} currentLink String @values "https://abc.com/"
@@ -183,8 +180,8 @@ const isAllowedFailingStatus = (currentLink) => {
 const checkLinks = (currentLink, region) => {
   if (
     currentLink &&
-    !isLinkExcluded("Check", currentLink) &&
-    currentLink != ""
+    !isLinkExcluded('Check', currentLink) &&
+    currentLink != ''
   ) {
     cy.request({
       url: currentLink,
@@ -211,18 +208,18 @@ const checkLinks = (currentLink, region) => {
     })
     if (
       currentLink &&
-      !isLinkExcluded("Visit", currentLink) &&
+      !isLinkExcluded('Visit', currentLink) &&
       isLinkValid(currentLink)
     ) {
-        let regionLink = currentLink
+      let regionLink = currentLink
       if (region) {
-        if (currentLink.endsWith("/")) {
+        if (currentLink.endsWith('/')) {
           regionLink = `${currentLink.slice(0, -1)}${region}`
         } else {
-          regionLink = `${currentLink}${region}`;
+          regionLink = `${currentLink}${region}`
         }
       }
-      cy.c_visitResponsive(regionLink, { size: "desktop" })
+      cy.c_visitResponsive(regionLink, { size: 'desktop' })
       linkDetails.visitedLinks.push(currentLink)
       cy.document().then((doc) => {
         const pageFailed = doc.querySelector('img[alt="Page not found"]')
@@ -239,58 +236,58 @@ const checkLinks = (currentLink, region) => {
           )
         }
       })
-      cy.get("a").each((availableLink) => {
-        const currentLink = availableLink.prop("href")
-        if (region){
-            checkLinks(currentLink, region)
+      cy.get('a').each((availableLink) => {
+        const currentLink = availableLink.prop('href')
+        if (region) {
+          checkLinks(currentLink, region)
         } else {
-            checkLinks(currentLink)
+          checkLinks(currentLink)
         }
       })
     }
   }
-};
+}
 
-describe("QATEST-96657 - Check URL in deriv.com", () => {
+describe('QATEST-96657 - Check URL in deriv.com', () => {
   before(() => {
     cy.clearAllSessionStorage()
     cy.clearAllLocalStorage()
     cy.clearAllCookies()
     cy.intercept({ resourceType: /xhr|fetch/ }, { log: false }) // to have cleaner logs for this test
   })
-  it("should validate each Url in deriv.com", () => {
-    cy.c_visitResponsive(Cypress.env("RegionDIEL"), { size: "desktop" })
-    linkDetails.visitedLinks.push(`${Cypress.env("RegionDIEL")}`)
-    cy.get("a").each((availableLink) => {
-      const currentLink = availableLink.prop("href")
-      checkLinks(currentLink, Cypress.env("RegionDIEL"))
+  it('should validate each Url in deriv.com', () => {
+    cy.c_visitResponsive(Cypress.env('RegionDIEL'), { size: 'desktop' })
+    linkDetails.visitedLinks.push(`${Cypress.env('RegionDIEL')}`)
+    cy.get('a').each((availableLink) => {
+      const currentLink = availableLink.prop('href')
+      checkLinks(currentLink, Cypress.env('RegionDIEL'))
     })
-    cy.c_visitResponsive("", { size: "desktop" })
-    linkDetails.visitedLinks.push(`${Cypress.env("RegionROW")}`)
-    cy.get("a").each((availableLink) => {
-      const currentLink = availableLink.prop("href")
+    cy.c_visitResponsive('', { size: 'desktop' })
+    linkDetails.visitedLinks.push(`${Cypress.env('RegionROW')}`)
+    cy.get('a').each((availableLink) => {
+      const currentLink = availableLink.prop('href')
       checkLinks(currentLink)
     })
-    cy.c_visitResponsive(Cypress.env("RegionEU"), { size: "desktop" })
-    linkDetails.visitedLinks.push(`${Cypress.env("RegionEU")}`)
-    cy.get("a").each((availableLink) => {
-      const currentLink = availableLink.prop("href")
+    cy.c_visitResponsive(Cypress.env('RegionEU'), { size: 'desktop' })
+    linkDetails.visitedLinks.push(`${Cypress.env('RegionEU')}`)
+    cy.get('a').each((availableLink) => {
+      const currentLink = availableLink.prop('href')
       checkLinks(currentLink)
     })
     cy.then(() => {
       failedLinks.failedCheckLinks.sort()
-      let uniqueFailedCheckedLinks = [...new Set(failedLinks.failedCheckLinks)];
+      let uniqueFailedCheckedLinks = [...new Set(failedLinks.failedCheckLinks)]
       failedLinks.failedVisitLinks.sort()
-      let uniqueFailedVisitLinks = [...new Set(failedLinks.failedVisitLinks)];
-      failedLinks.failedCheckLinks = uniqueFailedCheckedLinks;
-      failedLinks.failedVisitLinks = uniqueFailedVisitLinks;
+      let uniqueFailedVisitLinks = [...new Set(failedLinks.failedVisitLinks)]
+      failedLinks.failedCheckLinks = uniqueFailedCheckedLinks
+      failedLinks.failedVisitLinks = uniqueFailedVisitLinks
     })
     cy.writeFile(
-      "cypress/full_extended_results/validateAllLinksFailures.json",
+      'cypress/full_extended_results/validateAllLinksFailures.json',
       failedLinks
     )
     cy.readFile(
-      "cypress/full_extended_results/validateAllLinksFailures.json"
+      'cypress/full_extended_results/validateAllLinksFailures.json'
     ).then((failedLinks) => {
       expect(
         failedLinks.failedVisitLinks.length +
