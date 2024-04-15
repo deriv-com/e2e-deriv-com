583e9245917fa511ad7718697f0b46b2243069fe
Indulekha <indulekha@regentmarkets.com>
2024-04-15 16:00:16 +0400

    webflow

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -111,3 +111,12 @@ Cypress.Commands.add('c_checkAllPartnerLinks', (urlDetails) => {
     cy.go('back')
   })
 })
+Cypress.Commands.add('c_captureScreenshot', (pageName, viewport) => {
+  if (viewport == 'mobile') {
+      cy.percySnapshot(pageName, { widths: [400] })
+  } else if (viewport == 'medium') {
+      cy.percySnapshot(pageName, { widths: [800] })
+  } else if (viewport == 'desktop') {
+      cy.percySnapshot(pageName, { widths: [3072] })
+  }
+})
