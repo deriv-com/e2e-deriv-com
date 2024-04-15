e97ddb3dede7bf5f21f35e26e03d06c22abb2db3
Indulekha <indulekha@regentmarkets.com>
2024-04-15 14:23:32 +0400

    [ci skip]

--- cypress/support/percy.js
+++ cypress/support/percy.js
@@ -1,8 +1,9 @@
-Cypress.Commands.add('snapshot',(pageName,viewport)=>{
-    if (viewport == 'mobile') {
-     cy.percySnapshot(pageName, { widths: [400] })
-    } else if (viewport == 'medium'){ 
-    cy.percySnapshot(pageName, { widths: [800] })}
-    else if (viewport == 'desktop'){
-        cy.percySnapshot(pageName, { widths: [3072] })} 
-    })
+Cypress.Commands.add('snapshot', (pageName, viewport) => {
+    if (viewport === 'mobile') {
+        cy.percySnapshot(pageName, { widths: [400] })
+    } else if (viewport === 'medium') {
+        cy.percySnapshot(pageName, { widths: [800] })
+    } else if (viewport === 'desktop') {
+        cy.percySnapshot(pageName, { widths: [3072] })
+    }
+})
