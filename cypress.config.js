97aa2d47a972fc9f24e4918acda2af9af0214ed9
Indulekha <indulekha@regentmarkets.com>
2024-04-17 20:12:12 +0400

    [ci skip]

--- cypress.config.js
+++ cypress.config.js
@@ -45,7 +45,6 @@ module.exports = defineConfig({
     citizenship: process.env.CITIZENSHIP,
     user_password: process.env.USERPASSWORD,
     diel_country_list: ['Ecuador', 'South Africa', 'Brazil', 'Sri Lanka', 'Uruguay', 'Switzerland'],
-    webflow_baseUrl: process.env.WEBFLOW_BASE_URL,
     webflow_env: process.env.WEBFLOW_ENV
   },
   retries: {
