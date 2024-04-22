1fb33bbda343e2a91aafa02132d356b02fdad5d7
Rivitha <104499245+infanta-deriv@users.noreply.github.com>
2024-04-22 15:24:48 +0400

    webflowsmoke (#204)
    * webflowsmoke
    
    * webflow

---
+++ cypress/e2e/webflow/smoke/rowLivePricing.cy.js
@@ -0,0 +1,39 @@
+import '@testing-library/cypress/add-commands'
+
+describe('QATEST-1320 - Live Pricing table ROW', () => {
+  it(
+    'should click on all the 6 different markets and check whether the live pricing table for each is displayed.',
+    { tags: ['@smoke-tests', '@row-tests'] },
+    () => {
+      cy.c_visitResponsive('', { waitLoad: true, size: 'desktop' })
+      cy.c_validateMarkets('row')
+      cy.c_checkBuySellButton()
+    }
+  )
+
+  it(
+    'should click on View All link on all the 6 markets and check whether all instruments for the particular market is displayed - ROW desktop.',
+    { tags: ['@smoke-tests', '@row-tests'] },
+    () => {
+      cy.c_forexViewall('row', 'desk')
+      cy.c_derivedindicesViewall('row', 'desk')
+      cy.c_stockindicesViewall('row', 'desk')
+      cy.c_cryptocurrenciesViewall('row', 'desk')
+      cy.c_commoditiesViewall('row', 'desk')
+      cy.c_etfViewall('row', 'desk')
+    }
+  )
+
+  it(
+    'should click on View All link on all the 5 markets and check whether all instruments for the particular market is displayed - ROW mobile.',
+    { tags: ['@smoke-tests', '@row-tests'] },
+    () => {
+      cy.c_forexViewall('row', 'mob')
+      cy.c_derivedindicesViewall('row', 'mob')
+      cy.c_stockindicesViewall('row', 'mob')
+      cy.c_cryptocurrenciesViewall('row', 'mob')
+      cy.c_commoditiesViewall('row', 'mob')
+      cy.c_etfViewall('row', 'mob')
+    }
+  )
+})
