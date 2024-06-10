66abd82717a05babd80f4aff039c11a600ea922a
Indulekha <indulekha@regentmarkets.com>
2024-06-10 16:39:17 +0400

    webflow update urls

--- cypress/e2e/webflow/Percy/allPagesSnapshots.cy.js
+++ cypress/e2e/webflow/Percy/allPagesSnapshots.cy.js
@@ -14,24 +14,31 @@ function mapUrlForSnapshot(url) {
   }
 }
 
-function snapshot(pageName) {
+function snapshot(pageName,fullUrl) {
   cy.viewport('iphone-xr')
   cy.scrollTo('bottom', { ensureScrollable: false, duration: 2000 })
-  if (webflowbaseUrl.includes(webflow)) {
+  if (webflowbaseUrl.includes(webflow)&& !fullUrl.includes("404")) {
     cy.get('.new-navbar_component-wrapper')
       .eq(0)
       .scrollIntoView({ duration: 2000 })
+      .then(() => {
+        cy.wait(1000)
+      })
   }
   cy.percySnapshot(pageName)
   cy.viewport('macbook-16')
   cy.scrollTo('bottom', { ensureScrollable: false, duration: 2000 })
-  if (webflowbaseUrl.includes(webflow)) {
+  if (webflowbaseUrl.includes(webflow) && !fullUrl.includes("404")){
     cy.get('.new-navbar_component-wrapper')
       .eq(0)
       .scrollIntoView({ duration: 2000 })
+      .then(() => {
+        cy.wait(1000)
+      })
   }
   cy.percySnapshot(pageName)
 }
+
 
 describe('Visit URLs and Capture Percy Snapshots', () => {
   urls.urls.forEach((url) => {
@@ -41,7 +48,15 @@ describe('Visit URLs and Capture Percy Snapshots', () => {
       cy.log('WEBFLOW_ENV value:', Cypress.env('webflow_env'))
       cy.visit(fullUrl)
       pageName = mapUrlForSnapshot(url)
-      snapshot(pageName)
+      snapshot(pageName,fullUrl)
+      cy.log('the url is '+ cy.url)
+      if(url.includes('options')){
+        const optionsType = Cypress.env('optionsType')
+        optionsType.forEach(option => {
+          cy.findByRole('tab', { name: option }).click()
+          cy.log('clicked on tab' + option)
+          pageName = mapUrlForSnapshot(url) + option
+          snapshot(pageName,fullUrl)})}
     })
   })
 })
