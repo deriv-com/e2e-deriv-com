f64afe12de7d69205b1a5936b6d4a8c6d0964fe5
Rivitha <104499245+infanta-deriv@users.noreply.github.com>
2024-06-20 07:41:52 +0400

    updatetestcase (#224)

--- cypress/e2e/smoke/allSignUps.cy.js
+++ cypress/e2e/smoke/allSignUps.cy.js
@@ -15,11 +15,28 @@ describe('QATEST-1378 - should validate signup page', () => {
     signUpPage.elements.logInTxt().should('be.visible')
   }
 
+  function validateBannerContentImage() {
+    cy.findByRole('heading', {
+      name: 'Join over 2.5 million online traders',
+    }).should('be.visible')
+    cy.findByRole('img', { name: 'call to action background' }).should(
+      'be.visible'
+    )
+  }
+
   it(
     'should be able to sign up on a ROW mobile',
     { tags: ['@smoke-tests', '@row-tests'] },
     () => {
-      cy.c_visitResponsive('/signup')
+      cy.c_visitResponsive('/', { waitLoad: true })
+      validateBannerContentImage()
+      cy.get('#cta-container')
+        .contains('Open demo account')
+        .should('not.be.visible')
+      cy.findAllByRole('button', { name: 'Open demo account' })
+        .eq(0)
+        .should('be.visible')
+        .click({ force: true })
       validateSignUpPage()
     }
   )
@@ -28,7 +45,17 @@ describe('QATEST-1378 - should validate signup page', () => {
     'should be able to sign up on a EU mobile',
     { tags: ['@smoke-tests', '@eu-tests'] },
     () => {
-      cy.c_visitResponsive(Cypress.env('RegionEU') + '/signup')
+      cy.c_visitResponsive(Cypress.env('RegionEU') + '/', {
+        waitLoad: true,
+      })
+      validateBannerContentImage()
+      cy.get('#cta-container')
+        .contains('Open demo account')
+        .should('not.be.visible')
+      cy.findAllByRole('button', { name: 'Open demo account' })
+        .eq(0)
+        .should('be.visible')
+        .click({ force: true })
       validateSignUpPage()
       footer.elements.cfdFloatingBannerLink().should('exist')
     }
@@ -38,7 +65,12 @@ describe('QATEST-1378 - should validate signup page', () => {
     'should be able to sign up on a ROW desktop',
     { tags: ['@smoke-tests', '@row-tests'] },
     () => {
-      cy.c_visitResponsive('/signup/', { size: 'desktop' })
+      cy.c_visitResponsive('/', { waitLoad: true, size: 'desktop' })
+      validateBannerContentImage()
+      cy.findAllByRole('button', { name: 'Open demo account' })
+        .eq(3)
+        .should('be.visible')
+        .click({ force: true })
       validateSignUpPage()
     }
   )
@@ -47,9 +79,15 @@ describe('QATEST-1378 - should validate signup page', () => {
     'should be able to sign up on a EU desktop',
     { tags: ['@smoke-tests', '@eu-tests'] },
     () => {
-      cy.c_visitResponsive(Cypress.env('RegionEU') + '/signup/', {
+      cy.c_visitResponsive(Cypress.env('RegionEU') + '/', {
         size: 'desktop',
+        waitLoad: true,
       })
+      validateBannerContentImage()
+      cy.findAllByRole('button', { name: 'Open demo account' })
+        .eq(3)
+        .should('be.visible')
+        .click({ force: true })
       validateSignUpPage()
       footer.elements.cfdFloatingBannerLink().should('exist')
     }
