40cccb4fa936e1e0085091b5c7f80cefe919cde7
Indulekha <indulekha@regentmarkets.com>
2024-06-26 12:18:09 +0400

    add new urls

--- cypress/e2e/webflow/Percy/allPagesSnapshot.cy.js
+++ cypress/e2e/webflow/Percy/allPagesSnapshot.cy.js
@@ -7,7 +7,6 @@ const webflowbaseUrl = Cypress.env('webflow_env')
 const webflow = 'webflow'
 
 function mapUrlForSnapshot(url) {
-  cy.log('the url is' + url)
   if (url === '/') {
     return 'home'
   } else {
@@ -17,39 +16,38 @@ function mapUrlForSnapshot(url) {
 }
 
 function snapshot(pageName,fullUrl) {
-  cy.viewport('iphone-xr')
-  cy.scrollTo('bottom', { ensureScrollable: false, duration: 2000 })
-  if (webflowbaseUrl.includes(webflow)&& !fullUrl.includes("404")) {
-    cy.get('.new-navbar_component-wrapper')
-      .eq(0)
-      .scrollIntoView({ duration: 2000 })
-      .then(() => {
-        cy.wait(1000)
-      })
-  }
-  cy.percySnapshot(pageName)
+ // cy.viewport('iphone-xr')
+ // cy.scrollTo('bottom', { ensureScrollable: false, duration: 2000 })
+ // if (webflowbaseUrl.includes(webflow)&& !fullUrl.includes("404")) {
+ //   cy.get('.new-navbar_component-wrapper')
+  //    .eq(0)
+   //   .scrollIntoView({ duration: 2000 })
+    //  .then(() => {
+     //   cy.wait(1000)
+      //})
+  //}
+  //cy.percySnapshot(pageName)
   cy.viewport('macbook-16')
   cy.scrollTo('bottom', { ensureScrollable: false, duration: 2000 })
   if (webflowbaseUrl.includes(webflow) && !fullUrl.includes("404")){
-    cy.get('.new-navbar_component-wrapper')
+    cy.get('.new-navbar_component-wrapper',{ timeout: 60000 })
       .eq(0)
       .scrollIntoView({ duration: 2000 })
       .then(() => {
         cy.wait(1000)
       })
   }
-  cy.percySnapshot(pageName)
+  cy.percySnapshot(pageName,)
 }
 
 
 describe('Visit URLs and Capture Percy Snapshots', () => {
-  cy.log('webflow link is ' + webflowbaseUrl)
   urls.urls.forEach((url) => {
     let pageName
     const fullUrl = `${webflowbaseUrl}${url}`
     it(`Visits ${fullUrl} and captures Percy snapshot`, () => {
       cy.log('WEBFLOW_ENV value:', Cypress.env('webflow_env'))
-      cy.visit(fullUrl)
+      cy.visit(fullUrl,{ timeout: 60000 }).then(() => {
       pageName = mapUrlForSnapshot(url)
       snapshot(pageName,fullUrl)
       cy.log('the url is '+ cy.url)
@@ -60,6 +58,7 @@ describe('Visit URLs and Capture Percy Snapshots', () => {
           cy.log('clicked on tab' + option)
           pageName = mapUrlForSnapshot(url) + option
           snapshot(pageName,fullUrl)})}
+        })
     })
   })
 })
